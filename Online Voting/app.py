5304832a735f3a94433c5061e2aefd6e8be2eedc
RaviTeja9459 <smra002233@gmail.com>
2025-07-18 20:54:12 +0530

    app.py

--- Online Voting/app.py
+++ Online Voting/app.py
@@ -10,8 +10,8 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USE_TLS'] = True
-app.config['MAIL_USERNAME'] = 'mbkatyainidevarapu999@gmail.com'          # your Gmail
-app.config['MAIL_PASSWORD'] = 'jorw vgku zehy bksz'             # app password
+app.config['MAIL_USERNAME'] = ''          # your Gmail
+app.config['MAIL_PASSWORD'] = ''             # app password
 mail = Mail(app)
 
 # Database Initialization
